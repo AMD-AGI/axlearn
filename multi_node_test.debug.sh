8746811fe08d2073102712e615b44de46c4203c7
Akash Haridas <akash.haridas@amd.com>
2025-03-26 10:41:44 -0700

    update multinode script

--- multi_node_test.debug.sh
+++ multi_node_test.debug.sh
@@ -1,24 +1,23 @@
 export NCCL_IB_TC=41
-export NCCL_IB_TIMEOUT=17
 export NCCL_IB_SL=0
-# export NCCL_SOCKET_IFNAME=ens51f0np0
+# export NCCL_DEBUG=INFO
+# export NCCL_DEBUG_SUBSYS=NET # for NCCL debug, use COLL for collectives
+# export NCCL_DEBUG_FILE=llama3-70b-64N-short-run.%h.%p.log
 export NCCL_CHECKS_DISABLE=1
-export NCCL_IB_HCA=rdma0,rdma1,rdma2,rdma3,rdma4,rdma5,rdma6,rdma7
+export NCCL_IB_HCA=mlx5_0,mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_7,mlx5_8,mlx5_9  #### should be detected automatically.
+export NCCL_SOCKET_IFNAME=rdma0 #### should be detected automatically.
 export NCCL_IB_GID_INDEX=3
 export NCCL_CROSS_NIC=0
 export NCCL_PROTO=Simple
 
-# NCCL debug output
-export NCCL_DEBUG=INFO
-export NCCL_DEBUG_SUBSYS=INIT,COLL
-
-
+export LD_LIBRARY_PATH=/opt/rocm/lib:$LD_LIBRARY_PATH
 export HSA_FORCE_FINE_GRAIN_PCIE=1
 export TF_CPP_VMODULE=hlo_rematerialization=1,gpu_compiler=1,gpu_compiler=2,gpu_compiler=3,gpu_compiler=4,gpu_compiler=5
 export GPU_MAX_HW_QUEUES=2
 export HIP_FORCE_DEV_KERNARG=1
-export XLA_FLAGS="--xla_gpu_enable_latency_hiding_scheduler=true --xla_gpu_enable_cublaslt=True --xla_gpu_graph_level=0 --xla_gpu_autotune_level=5"
-export XLA_PYTHON_CLIENT_MEM_FRACTION=0.95
+# export XLA_FLAGS="--xla_gpu_enable_cublaslt=True --xla_gpu_graph_level=0 --xla_gpu_autotune_level=5 --xla_gpu_enable_latency_hiding_scheduler=true"
+export XLA_FLAGS="--xla_gpu_enable_cublaslt=True --xla_gpu_graph_level=0 --xla_gpu_autotune_level=5 --xla_gpu_enable_latency_hiding_scheduler=true --xla_gpu_enable_reduce_scatter_combine_by_dim=false --xla_gpu_reduce_scatter_combine_threshold_bytes=8589934592 --xla_gpu_all_reduce_combine_threshold_bytes=8589934592 --xla_gpu_all_gather_combine_threshold_bytes=137438953472 --xla_gpu_enable_all_gather_combine_by_dim=FALSE"
+export XLA_PYTHON_CLIENT_MEM_FRACTION=0.975
 export NVTE_ALLOW_NONDETERMINISTIC_ALGO=1
 export NVTE_FUSED_ATTN=1
 export NVTE_FUSED_ATTN_CK=1
@@ -38,8 +37,6 @@ export NVTE_CK_HOW_V3_BF16_CVT=2 # may want to try 1 and 0 if we get NAN loss in
 export DISTRIBUTED_COORDINATOR=$HEAD_NODE:12345
 export NUM_PROCESSES=2
 
-
-export LD_LIBRARY_PATH=/opt/rocm/lib:$LD_LIBRARY_PATH
 
 # XLA_FLAGS=--xla_dump_to=/tmp/xla_dump; \
 mkdir -p /tmp/gpt_c4_test; \
